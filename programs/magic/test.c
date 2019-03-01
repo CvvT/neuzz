1c09ba9225abf22da114841ed0dab0ea8b63f099
CvvT <weitengchencc@gmail.com>
2019-02-28 20:34:42 -0800

    add new test program

---
+++ programs/magic/test.c
@@ -0,0 +1,621 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#define N 100
+int main(int argc, char *argv[]) {
+
+  char str[N+1];
+  int len;
+  fgets(str, N+1, stdin);
+  len = strlen(str);
+  if (len < N) {
+    return 1;
+  }
+
+  
+
+  if (str[0] != 'c') {
+  	printf("failed 0\n");
+  	return 1;
+  }
+
+
+  if (str[1] != 'o') {
+  	printf("failed 1\n");
+  	return 1;
+  }
+
+
+  if (str[2] != 'x') {
+  	printf("failed 2\n");
+  	return 1;
+  }
+
+
+  if (str[3] != 'p') {
+  	printf("failed 3\n");
+  	return 1;
+  }
+
+
+  if (str[4] != 'y') {
+  	printf("failed 4\n");
+  	return 1;
+  }
+
+
+  if (str[5] != 'p') {
+  	printf("failed 5\n");
+  	return 1;
+  }
+
+
+  if (str[6] != 'e') {
+  	printf("failed 6\n");
+  	return 1;
+  }
+
+
+  if (str[7] != 'c') {
+  	printf("failed 7\n");
+  	return 1;
+  }
+
+
+  if (str[8] != 'v') {
+  	printf("failed 8\n");
+  	return 1;
+  }
+
+
+  if (str[9] != 'r') {
+  	printf("failed 9\n");
+  	return 1;
+  }
+
+
+  if (str[10] != 'q') {
+  	printf("failed 10\n");
+  	return 1;
+  }
+
+
+  if (str[11] != 'e') {
+  	printf("failed 11\n");
+  	return 1;
+  }
+
+
+  if (str[12] != 'k') {
+  	printf("failed 12\n");
+  	return 1;
+  }
+
+
+  if (str[13] != 'g') {
+  	printf("failed 13\n");
+  	return 1;
+  }
+
+
+  if (str[14] != 'v') {
+  	printf("failed 14\n");
+  	return 1;
+  }
+
+
+  if (str[15] != 'p') {
+  	printf("failed 15\n");
+  	return 1;
+  }
+
+
+  if (str[16] != 'h') {
+  	printf("failed 16\n");
+  	return 1;
+  }
+
+
+  if (str[17] != 'u') {
+  	printf("failed 17\n");
+  	return 1;
+  }
+
+
+  if (str[18] != 'f') {
+  	printf("failed 18\n");
+  	return 1;
+  }
+
+
+  if (str[19] != 'z') {
+  	printf("failed 19\n");
+  	return 1;
+  }
+
+
+  if (str[20] != 'v') {
+  	printf("failed 20\n");
+  	return 1;
+  }
+
+
+  if (str[21] != 'l') {
+  	printf("failed 21\n");
+  	return 1;
+  }
+
+
+  if (str[22] != 'u') {
+  	printf("failed 22\n");
+  	return 1;
+  }
+
+
+  if (str[23] != 'j') {
+  	printf("failed 23\n");
+  	return 1;
+  }
+
+
+  if (str[24] != 'w') {
+  	printf("failed 24\n");
+  	return 1;
+  }
+
+
+  if (str[25] != 'r') {
+  	printf("failed 25\n");
+  	return 1;
+  }
+
+
+  if (str[26] != 'l') {
+  	printf("failed 26\n");
+  	return 1;
+  }
+
+
+  if (str[27] != 'p') {
+  	printf("failed 27\n");
+  	return 1;
+  }
+
+
+  if (str[28] != 't') {
+  	printf("failed 28\n");
+  	return 1;
+  }
+
+
+  if (str[29] != 'a') {
+  	printf("failed 29\n");
+  	return 1;
+  }
+
+
+  if (str[30] != 'n') {
+  	printf("failed 30\n");
+  	return 1;
+  }
+
+
+  if (str[31] != 'h') {
+  	printf("failed 31\n");
+  	return 1;
+  }
+
+
+  if (str[32] != 'z') {
+  	printf("failed 32\n");
+  	return 1;
+  }
+
+
+  if (str[33] != 'b') {
+  	printf("failed 33\n");
+  	return 1;
+  }
+
+
+  if (str[34] != 's') {
+  	printf("failed 34\n");
+  	return 1;
+  }
+
+
+  if (str[35] != 'u') {
+  	printf("failed 35\n");
+  	return 1;
+  }
+
+
+  if (str[36] != 'n') {
+  	printf("failed 36\n");
+  	return 1;
+  }
+
+
+  if (str[37] != 'f') {
+  	printf("failed 37\n");
+  	return 1;
+  }
+
+
+  if (str[38] != 'e') {
+  	printf("failed 38\n");
+  	return 1;
+  }
+
+
+  if (str[39] != 'q') {
+  	printf("failed 39\n");
+  	return 1;
+  }
+
+
+  if (str[40] != 'g') {
+  	printf("failed 40\n");
+  	return 1;
+  }
+
+
+  if (str[41] != 'p') {
+  	printf("failed 41\n");
+  	return 1;
+  }
+
+
+  if (str[42] != 'l') {
+  	printf("failed 42\n");
+  	return 1;
+  }
+
+
+  if (str[43] != 'm') {
+  	printf("failed 43\n");
+  	return 1;
+  }
+
+
+  if (str[44] != 'f') {
+  	printf("failed 44\n");
+  	return 1;
+  }
+
+
+  if (str[45] != 't') {
+  	printf("failed 45\n");
+  	return 1;
+  }
+
+
+  if (str[46] != 'u') {
+  	printf("failed 46\n");
+  	return 1;
+  }
+
+
+  if (str[47] != 'i') {
+  	printf("failed 47\n");
+  	return 1;
+  }
+
+
+  if (str[48] != 'l') {
+  	printf("failed 48\n");
+  	return 1;
+  }
+
+
+  if (str[49] != 'b') {
+  	printf("failed 49\n");
+  	return 1;
+  }
+
+
+  if (str[50] != 'u') {
+  	printf("failed 50\n");
+  	return 1;
+  }
+
+
+  if (str[51] != 'r') {
+  	printf("failed 51\n");
+  	return 1;
+  }
+
+
+  if (str[52] != 'r') {
+  	printf("failed 52\n");
+  	return 1;
+  }
+
+
+  if (str[53] != 'e') {
+  	printf("failed 53\n");
+  	return 1;
+  }
+
+
+  if (str[54] != 'n') {
+  	printf("failed 54\n");
+  	return 1;
+  }
+
+
+  if (str[55] != 'z') {
+  	printf("failed 55\n");
+  	return 1;
+  }
+
+
+  if (str[56] != 'j') {
+  	printf("failed 56\n");
+  	return 1;
+  }
+
+
+  if (str[57] != 'v') {
+  	printf("failed 57\n");
+  	return 1;
+  }
+
+
+  if (str[58] != 'g') {
+  	printf("failed 58\n");
+  	return 1;
+  }
+
+
+  if (str[59] != 'z') {
+  	printf("failed 59\n");
+  	return 1;
+  }
+
+
+  if (str[60] != 'a') {
+  	printf("failed 60\n");
+  	return 1;
+  }
+
+
+  if (str[61] != 'e') {
+  	printf("failed 61\n");
+  	return 1;
+  }
+
+
+  if (str[62] != 'u') {
+  	printf("failed 62\n");
+  	return 1;
+  }
+
+
+  if (str[63] != 'b') {
+  	printf("failed 63\n");
+  	return 1;
+  }
+
+
+  if (str[64] != 'b') {
+  	printf("failed 64\n");
+  	return 1;
+  }
+
+
+  if (str[65] != 'd') {
+  	printf("failed 65\n");
+  	return 1;
+  }
+
+
+  if (str[66] != 'z') {
+  	printf("failed 66\n");
+  	return 1;
+  }
+
+
+  if (str[67] != 'f') {
+  	printf("failed 67\n");
+  	return 1;
+  }
+
+
+  if (str[68] != 'h') {
+  	printf("failed 68\n");
+  	return 1;
+  }
+
+
+  if (str[69] != 'w') {
+  	printf("failed 69\n");
+  	return 1;
+  }
+
+
+  if (str[70] != 'o') {
+  	printf("failed 70\n");
+  	return 1;
+  }
+
+
+  if (str[71] != 'd') {
+  	printf("failed 71\n");
+  	return 1;
+  }
+
+
+  if (str[72] != 's') {
+  	printf("failed 72\n");
+  	return 1;
+  }
+
+
+  if (str[73] != 'f') {
+  	printf("failed 73\n");
+  	return 1;
+  }
+
+
+  if (str[74] != 'v') {
+  	printf("failed 74\n");
+  	return 1;
+  }
+
+
+  if (str[75] != 's') {
+  	printf("failed 75\n");
+  	return 1;
+  }
+
+
+  if (str[76] != 'v') {
+  	printf("failed 76\n");
+  	return 1;
+  }
+
+
+  if (str[77] != 't') {
+  	printf("failed 77\n");
+  	return 1;
+  }
+
+
+  if (str[78] != 'x') {
+  	printf("failed 78\n");
+  	return 1;
+  }
+
+
+  if (str[79] != 'a') {
+  	printf("failed 79\n");
+  	return 1;
+  }
+
+
+  if (str[80] != 'h') {
+  	printf("failed 80\n");
+  	return 1;
+  }
+
+
+  if (str[81] != 'h') {
+  	printf("failed 81\n");
+  	return 1;
+  }
+
+
+  if (str[82] != 'r') {
+  	printf("failed 82\n");
+  	return 1;
+  }
+
+
+  if (str[83] != 'h') {
+  	printf("failed 83\n");
+  	return 1;
+  }
+
+
+  if (str[84] != 'z') {
+  	printf("failed 84\n");
+  	return 1;
+  }
+
+
+  if (str[85] != 'u') {
+  	printf("failed 85\n");
+  	return 1;
+  }
+
+
+  if (str[86] != 'v') {
+  	printf("failed 86\n");
+  	return 1;
+  }
+
+
+  if (str[87] != 'f') {
+  	printf("failed 87\n");
+  	return 1;
+  }
+
+
+  if (str[88] != 'l') {
+  	printf("failed 88\n");
+  	return 1;
+  }
+
+
+  if (str[89] != 'h') {
+  	printf("failed 89\n");
+  	return 1;
+  }
+
+
+  if (str[90] != 'q') {
+  	printf("failed 90\n");
+  	return 1;
+  }
+
+
+  if (str[91] != 'i') {
+  	printf("failed 91\n");
+  	return 1;
+  }
+
+
+  if (str[92] != 'a') {
+  	printf("failed 92\n");
+  	return 1;
+  }
+
+
+  if (str[93] != 'z') {
+  	printf("failed 93\n");
+  	return 1;
+  }
+
+
+  if (str[94] != 'z') {
+  	printf("failed 94\n");
+  	return 1;
+  }
+
+
+  if (str[95] != 'x') {
+  	printf("failed 95\n");
+  	return 1;
+  }
+
+
+  if (str[96] != 'w') {
+  	printf("failed 96\n");
+  	return 1;
+  }
+
+
+  if (str[97] != 'j') {
+  	printf("failed 97\n");
+  	return 1;
+  }
+
+
+  if (str[98] != 'r') {
+  	printf("failed 98\n");
+  	return 1;
+  }
+
+
+  if (str[99] != 'm') {
+  	printf("failed 99\n");
+  	return 1;
+  }
+
+
+  assert(0 && "assertion failed");
+  return 0;
+}
